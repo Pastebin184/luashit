e45d0987716ae285fdf812b568803d947716b327
Pastebin <77293365+Pastebin184@users.noreply.github.com>
2022-09-20 22:24:27 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pastebin184/luashit/main/req.lua')))()
+wait(5)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pastebin184/luashit/main/internal.lua')))()
